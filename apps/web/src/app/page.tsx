c38215cf48d57ee1690fbaaa9ea1ea263389b982
vadym10-sys <romaniukvadym10@gmail.com>
2026-06-21 14:44:58 +0200

    Disable static caching for SaaS shell

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,8 @@
 import DashboardClient from "./dashboard-client";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function DashboardPage() {
   return <DashboardClient />;
 }
-
